332fcc5368f2500d83049152e514f7d1872eb0f4
Matteo Bettini <matteo1.bettini@mail.polimi.it>
2020-03-03 17:29:03 +0100

    Added sample main and test

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
