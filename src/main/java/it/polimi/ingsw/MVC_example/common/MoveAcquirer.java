e30f6c7fdad4cb9909f3bdefe1c6cdedfa0bceed
Mirko De Vita <mirko@MBP-di-Mirko.lan>
2020-03-27 23:50:17 +0100

    Started working on model

--- src/main/java/it/polimi/ingsw/MVC_example/common/MoveAcquirer.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.MVC_example.common;
-
-@Deprecated
-public interface MoveAcquirer {
-
-    public void send(int a, int b);
-
-}
---
+++ src/main/java/it/polimi/ingsw/model/signals/NoBuildingSignal.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.model.signals;
+
+public class NoBuildingSignal {
+}
