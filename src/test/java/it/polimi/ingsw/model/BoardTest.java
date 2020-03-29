f4b996e1a88f55155acb78bb94989aff4bd29630
MirkoDeVita98 <mirko.devita@icloud.com>
2020-03-29 19:02:45 +0200

    Added some tests for the Board class. Modified the UML according to the previous changes in the code.

--- src/test/java/it/polimi/ingsw/model/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -9,6 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+    /**
+     * Test if all the Cells in the Board are not null.
+     * Test if the getCell method returns only Cell in the correct position.
+     * Test if the getCell returns null if the given position is out of Board's bounds.
+     */
     @Test
     void testGetters(){
         Board boardT = new Board();
@@ -36,29 +41,37 @@ class BoardTest {
 
     }
 
+    /**
+     * Test if it is possible to build provided that there are enough buildings.
+     * Test if the availableBuildings and restockBuilding methods return the correct numbers.
+     */
     @Test
     void testUseBuilding(){
         Board boardT = new Board();
 
         //If there is availability the building can be used and the availability decreases by 1.
+        assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR),22);
         for(int i = 1; i <= 22; ++i){
             assertTrue(boardT.canUseBuilding(BuildingType.FIRST_FLOOR));
             assertTrue(boardT.useBuilding(BuildingType.FIRST_FLOOR));
             assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR),22 - i);
         }
 
+        assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR),18);
         for(int i = 1; i <= 18; ++i){
             assertTrue(boardT.canUseBuilding(BuildingType.SECOND_FLOOR));
             assertTrue(boardT.useBuilding(BuildingType.SECOND_FLOOR));
             assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR),18 - i);
         }
 
+        assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR),14);
         for(int i = 1; i <= 14; ++i){
             assertTrue(boardT.canUseBuilding(BuildingType.THIRD_FLOOR));
             assertTrue(boardT.useBuilding(BuildingType.THIRD_FLOOR));
             assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR),14 - i);
         }
 
+        assertEquals(boardT.availableBuildings(BuildingType.DOME),18);
         for(int i = 1; i <= 18; ++i){
             assertTrue(boardT.canUseBuilding(BuildingType.DOME));
             assertTrue(boardT.useBuilding(BuildingType.DOME));
@@ -81,6 +94,62 @@ class BoardTest {
         assertFalse(boardT.canUseBuilding(BuildingType.DOME));
         assertFalse(boardT.useBuilding(BuildingType.DOME));
         assertEquals(boardT.availableBuildings(BuildingType.DOME),0);
+
+        //Checks if the restock method works and only restocks the given building type.
+        boardT.restockBuilding(BuildingType.FIRST_FLOOR);
+        assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR),22);
+        assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR),0);
+        assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR),0);
+        assertEquals(boardT.availableBuildings(BuildingType.DOME),0);
+
+        boardT.restockBuilding(BuildingType.SECOND_FLOOR);
+        assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR),22);
+        assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR),18);
+        assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR),0);
+        assertEquals(boardT.availableBuildings(BuildingType.DOME),0);
+
+        boardT.restockBuilding(BuildingType.THIRD_FLOOR);
+        assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR),22);
+        assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR),18);
+        assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR),14);
+        assertEquals(boardT.availableBuildings(BuildingType.DOME),0);
+
+        boardT.restockBuilding(BuildingType.DOME);
+        assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR),22);
+        assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR),18);
+        assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR),14);
+        assertEquals(boardT.availableBuildings(BuildingType.DOME),18);
+
+
+    }
+
+    /**
+     * Test if the cloned Board equals the original one but it is not the same instance.
+     * Test if all the Cells have the same properties but they are not identical.
+     */
+    @Test
+    void testClone(){
+        Board boardT = new Board();
+
+        Board clonedBoard = boardT.clone();
+
+        assertNotSame(boardT, clonedBoard);
+        assertEquals(boardT,clonedBoard);
+
+        //Check that the number of available buildings stays the same.
+        assertEquals(boardT.availableBuildings(BuildingType.FIRST_FLOOR), clonedBoard.availableBuildings(BuildingType.FIRST_FLOOR));
+        assertEquals(boardT.availableBuildings(BuildingType.SECOND_FLOOR), clonedBoard.availableBuildings(BuildingType.SECOND_FLOOR));
+        assertEquals(boardT.availableBuildings(BuildingType.THIRD_FLOOR), clonedBoard.availableBuildings(BuildingType.THIRD_FLOOR));
+        assertEquals(boardT.availableBuildings(BuildingType.DOME), clonedBoard.availableBuildings(BuildingType.DOME));
+
+        //Check that all the Cells are the same but not identical.
+        for(int i = 0; i < 5; ++i){
+            for(int j = 0; j < 5; ++j){
+                Point p = new Point(i,j);
+                assertNotSame(boardT.getCell(p),clonedBoard.getCell(p));
+                assertEquals(boardT.getCell(p),clonedBoard.getCell(p));
+            }
+        }
     }
 
 }
---
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+}
