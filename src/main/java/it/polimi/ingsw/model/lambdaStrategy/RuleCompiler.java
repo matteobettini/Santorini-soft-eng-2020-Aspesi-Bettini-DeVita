0f5142e2fc57de2e0fa298e23c1ca63193a751d7
Matteo Bettini <matteo1.bettini@mail.polimi.it>
2020-03-29 12:15:06 +0200

    Finished implementation of class StatementCompiler

--- src/main/java/it/polimi/ingsw/model/lambdaStrategy/RuleCompiler.java
+++ src/main/java/it/polimi/ingsw/model/lambdaStrategy/RuleCompiler.java
@@ -1,8 +1,13 @@
 package it.polimi.ingsw.model.lambdaStrategy;
 
 import it.polimi.ingsw.cardReader.CardRule;
+import it.polimi.ingsw.cardReader.RuleStatement;
+import it.polimi.ingsw.cardReader.enums.TriggerType;
 import it.polimi.ingsw.model.InternalModel;
 import it.polimi.ingsw.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *  Translates a CardRule into a CompiledCardRule
@@ -18,7 +23,19 @@ public class RuleCompiler {
      * @return the compiled rule
      **/
     public static CompiledCardRule compile(InternalModel internalModel, CardRule cardRule, Player owner){
-        return null;
+
+        List<LambdaStatement> compiledStatements = new ArrayList<>();
+        LambdaEffect compiledEffect = null;
+        TriggerType trigger = cardRule.getTrigger();
+
+        for(RuleStatement s : cardRule.getStatements()){
+            compiledStatements.add(StatementCompiler.compileStatement(internalModel, s, owner));
+        }
+
+        compiledEffect = EffectCompiler.compileEffect(internalModel, cardRule.getEffect(), owner);
+
+        return new CompiledCardRule(compiledStatements, compiledEffect, trigger);
+
     }
 
 }
--- src/main/java/it/polimi/ingsw/model/lambdaStrategy/StatementCompiler.java
+++ src/main/java/it/polimi/ingsw/model/lambdaStrategy/StatementCompiler.java
@@ -2,19 +2,21 @@ package it.polimi.ingsw.model.lambdaStrategy;
 
 import it.polimi.ingsw.cardReader.RuleStatement;
 import it.polimi.ingsw.cardReader.enums.StatementType;
+import it.polimi.ingsw.cardReader.enums.StatementVerbType;
 import it.polimi.ingsw.model.InternalModel;
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.Worker;
 import it.polimi.ingsw.model.enums.BuildingType;
 import it.polimi.ingsw.model.enums.LevelType;
+import it.polimi.ingsw.model.enums.PlayerFlag;
 import it.polimi.ingsw.model.enums.PlayerState;
 import it.polimi.ingsw.model.exceptions.NoWorkerPresentException;
 import it.polimi.ingsw.model.turnInfo.BuildData;
 import it.polimi.ingsw.model.turnInfo.MoveData;
 
 import java.awt.*;
-import java.util.Iterator;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Translates a Rule Statement into a Compiled Statement
@@ -29,42 +31,186 @@ public class StatementCompiler {
      * @return the compiled statement
      */
     public static LambdaStatement compileStatement(InternalModel model, RuleStatement statement, Player owner) {
-        // TODO implement here
-        return null;
+       StatementVerbType verb = statement.getVerb();
+       LambdaStatement result = null;
+       switch (verb){
+           case PLAYER_EQUALS:
+               result = compilePlayerEquals(statement, owner);
+               break;
+           case STATE_EQUALS:
+               result = compileStateEquals(statement);
+               break;
+           case HAS_FLAG:
+               result = compileHasFlag(statement, owner);
+               break;
+           case MOVE_LENGTH:
+               result = compileMoveLegth(model, statement);
+               break;
+           case EXISTS_DELTA_MORE:
+               result = compileExistsDeltaMore(model, statement);
+               break;
+           case EXISTS_DELTA_LESS:
+               result = compileExistsDeltaLess(model, statement);
+               break;
+           case LEVEL_TYPE:
+               result = compileLevelType(model, statement);
+               break;
+           case INTERACTION_NUM:
+               result = compileInteractionNum(model, statement);
+               break;
+           case POSITION_EQUALS:
+               result = compilePositionEquals(model, statement);
+               break;
+           case BUILD_NUM:
+               result = compileBuildNum(statement);
+               break;
+           case BUILD_DOME_EXCEPT:
+               result = compileBuildDomeExcept(model, statement);
+               break;
+           case BUILD_DOME:
+               result = compileBuildDome(model, statement);
+               break;
+           case BUILD_IN_SAME_SPOT:
+               result = compileBuildInSameSpot(statement);
+               break;
+       }
+       return result;
     }
 
 
-    private static LambdaStatement compileHasFlag(InternalModel model, RuleStatement statement, Player owner) {
-        // TODO implement here
-        return null;
+    private static LambdaStatement compileHasFlag(RuleStatement statement, Player owner) {
+        LambdaStatement lambdaStatement = ((moveData, buildData) -> {
+            boolean result = false;
+            if(moveData == null){
+                if(statement.getSubject().equals("YOU")){
+                    result = buildData.getPlayer().hasFlag(PlayerFlag.valueOf(statement.getObject()));
+                }
+                else if(statement.getSubject().equals("CARD_OWNER")){
+                    result = owner.hasFlag(PlayerFlag.valueOf(statement.getObject()));
+                }
+            }
+            else if (buildData == null){
+                if(statement.getSubject().equals("YOU")){
+                    result = moveData.getPlayer().hasFlag(PlayerFlag.valueOf(statement.getObject()));
+                }
+                else if(statement.getSubject().equals("CARD_OWNER")){
+                    result = owner.hasFlag(PlayerFlag.valueOf(statement.getObject()));
+                }
+            }
+            if(statement.getType() == StatementType.NIF)
+                result = !result;
+
+            return result;
+        });
+        return  lambdaStatement;
     }
 
 
-    private static LambdaStatement compileMoveLegth(InternalModel model, RuleStatement statement, Player owner) {
-        // TODO implement here
-        return null;
+    private static LambdaStatement compileMoveLegth(InternalModel model, RuleStatement statement) {
+        LambdaStatement lambdaStatement = (moveData, buildData) -> {
+            boolean result = false;
+            assert (buildData == null);
+            List<Point> moves = moveData.getData();
+            assert (!moves.isEmpty());
+
+            if (moves.size() == Integer.parseInt(statement.getObject())) {
+
+                Point myPosition = moveData.getWorker().getPosition();
+                Point myFirstMovePosition = moves.get(0);
+                if (StatementCompiler.adiacent(myPosition, myFirstMovePosition) && model.getBoard().getCell(myFirstMovePosition).getTopBuilding() != LevelType.DOME){
+                    boolean correct = true;
+                    for (int i = 0; i < moves.size() - 1; i++) {
+                        if(!StatementCompiler.adiacent(moves.get(i), moves.get(i+1)) || model.getBoard().getCell(moves.get(i+1)).getTopBuilding() == LevelType.DOME)
+                            correct = false;
+                    }
+                    if(correct)
+                        result = true;
+                }
+             }
+
+            if(statement.getType() == StatementType.NIF)
+                result = !result;
+
+            return result;
+        };
+        return  lambdaStatement;
     }
 
-    private static LambdaStatement compileExistsDeltaMore(InternalModel model, RuleStatement statement, Player owner) {
-        // TODO implement here
-        return null;
+    private static LambdaStatement compileExistsDeltaMore(InternalModel model, RuleStatement statement) {
+        LambdaStatement lambdaStatement = (moveData, buildData) -> {
+            boolean result = false;
+            assert(buildData == null);
+
+            List<Point> moves = moveData.getData();
+            if(!moves.isEmpty()){
+                List<Integer> deltas = StatementCompiler.getMoveDeltas(model, moves, moveData);
+                int max = deltas.stream()
+                        .max(Integer::compareTo)
+                        .orElse(0);
+                if(max > Integer.parseInt(statement.getObject()))
+                    result = true;
+            }
+
+            if(statement.getType() == StatementType.NIF)
+                result = !result;
+
+            return result;
+        };
+        return  lambdaStatement;
     }
 
 
-    private static LambdaStatement compileExistsDeltaLess(InternalModel model, RuleStatement statement, Player owner) {
-        // TODO implement here
-        return null;
+    private static LambdaStatement compileExistsDeltaLess(InternalModel model, RuleStatement statement) {
+        LambdaStatement lambdaStatement = (moveData, buildData) -> {
+            boolean result = false;
+            assert(buildData == null);
+
+            List<Point> moves = moveData.getData();
+            if(!moves.isEmpty()){
+                List<Integer> deltas = StatementCompiler.getMoveDeltas(model, moves, moveData);
+                int min = deltas.stream()
+                        .min(Integer::compareTo)
+                        .orElse(0);
+                if(min < Integer.parseInt(statement.getObject()))
+                    result = true;
+            }
+
+            if(statement.getType() == StatementType.NIF)
+                result = !result;
+
+            return result;
+        };
+        return  lambdaStatement;
     }
 
 
-    private static LambdaStatement compileLevelType(InternalModel model, RuleStatement statement, Player owner) {
-        // TODO implement here
-        return null;
+    private static LambdaStatement compileLevelType(InternalModel model, RuleStatement statement) {
+        LambdaStatement lambdaStatement = (moveData, buildData) -> {
+            boolean result = false;
+            assert(buildData == null);
+            List<Point> moves = moveData.getData();
+
+            if(statement.getSubject().equals("START_POSITION")) {
+                Point startPosition = moves.get(0);
+                if(model.getBoard().getCell(startPosition).getTopBuilding() == LevelType.valueOf(statement.getObject()))
+                    result = true;
+            }
+            else if(statement.getSubject().equals("FINAL_POSITION")){
+                Point finalPosition = moves.get(moves.size()-1);
+                if(model.getBoard().getCell(finalPosition).getTopBuilding() == LevelType.valueOf(statement.getObject()))
+                    result = true;
+            }
+
+            if(statement.getType() == StatementType.NIF)
+                result = !result;
+
+            return result;
+        };
+        return  lambdaStatement;
     }
 
 
     private static LambdaStatement compileInteractionNum(InternalModel model, RuleStatement statement) {
-        //TODO
         LambdaStatement lambdaStatement = ((moveData, buildData) -> {
             boolean result = false;
             assert(buildData == null);
@@ -93,13 +239,28 @@ public class StatementCompiler {
     }
 
 
-    private static LambdaStatement compilePositionEquals(RuleStatement statement) {
+    private static LambdaStatement compilePositionEquals(InternalModel model, RuleStatement statement) {
         LambdaStatement lambdaStatement = ((moveData, buildData) -> {
             boolean result = false;
             assert(buildData == null);
             List<Point> moves = moveData.getData();
-            if(moves.get(moves.size()-1).equals(moveData.getWorker().getPosition()))
-                result = true;
+
+            if(statement.getObject().equals("START_POSITION"))
+                if(moves.get(moves.size()-1).equals(moveData.getWorker().getPosition()))
+                    result = true;
+
+            else if(statement.getObject().equals("OPPONENTS")){
+
+                Point finalPosition = moves.get(moves.size()-1);
+                List<Worker> myWorkers = moveData.getPlayer().getWorkers();
+                assert(myWorkers.size() == 2);
+                try{
+                    String presentWID = model.getBoard().getCell(finalPosition).getWorkerID();
+                    if(!presentWID.equals(myWorkers.get(0).getID()) && !presentWID.equals(myWorkers.get(1).getID()))
+                        result = true;
+                } catch (NoWorkerPresentException ignored){ }
+
+            }
 
             if(statement.getType() == StatementType.NIF)
                 result = !result;
@@ -112,18 +273,18 @@ public class StatementCompiler {
 
     private static LambdaStatement compileBuildNum(RuleStatement statement) {
         LambdaStatement lambdaStatement = ((moveData, buildData) -> {
-            boolean result = false;
+            boolean result;
             assert(moveData == null);
             Map<Point, BuildingType> builds = buildData.getData();
             if(builds.size() != Integer.parseInt(statement.getObject()))
-                return false;
-
-            result = true;
-            for (Point currPoint : builds.keySet()) {
-                if(!adiacent(currPoint, buildData.getWorker().getPosition()))
-                    result = false;
+                result = false;
+            else {
+                result = true;
+                for (Point currPoint : builds.keySet()) {
+                    if (!StatementCompiler.adiacent(currPoint, buildData.getWorker().getPosition()))
+                        result = false;
+                }
             }
-
             if(statement.getType() == StatementType.NIF)
                 result = !result;
 
@@ -137,15 +298,15 @@ public class StatementCompiler {
             boolean result = false;
             assert(moveData == null);
             Map<Point, BuildingType> builds = buildData.getData();
-            if(builds.isEmpty())
-                return false;
 
-            for (Point currPoint : builds.keySet()) {
-                if (builds.get(currPoint) == BuildingType.DOME)
-                    if (model.getBoard().getCell(currPoint).getTopBuilding() != LevelType.valueOf(statement.getObject())) {
-                        result = true;
-
-                    }
+            if(!builds.isEmpty()) {
+                for (Point currPoint : builds.keySet()) {
+                    if (builds.get(currPoint) == BuildingType.DOME)
+                        if (model.getBoard().getCell(currPoint).getTopBuilding() != LevelType.valueOf(statement.getObject())) {
+                            result = true;
+                            break;
+                        }
+                }
             }
 
             if(statement.getType() == StatementType.NIF)
@@ -162,17 +323,16 @@ public class StatementCompiler {
             boolean result = false;
             assert(moveData == null);
             Map<Point, BuildingType> builds = buildData.getData();
-            if(builds.isEmpty())
-                return false;
 
-            for (Point currPoint : builds.keySet()) {
-                if (builds.get(currPoint) == BuildingType.DOME)
-                    if (model.getBoard().getCell(currPoint).getTopBuilding() == LevelType.valueOf(statement.getObject())) {
-                        result = true;
-                        break;
-                    }
+            if(!builds.isEmpty()) {
+                for (Point currPoint : builds.keySet()) {
+                    if (builds.get(currPoint) == BuildingType.DOME)
+                        if (model.getBoard().getCell(currPoint).getTopBuilding() == LevelType.valueOf(statement.getObject())) {
+                            result = true;
+                            break;
+                        }
+                }
             }
-
             if(statement.getType() == StatementType.NIF)
                 result = !result;
 
@@ -184,21 +344,23 @@ public class StatementCompiler {
 
     private static LambdaStatement compileBuildInSameSpot(RuleStatement statement) {
         LambdaStatement lambdaStatement = ((moveData, buildData) -> {
-            boolean result = false;
-            if(moveData == null){
-                Map<Point, BuildingType> builds = buildData.getData();
-                if(builds.isEmpty())
-                    return false;
+            boolean result;
+            assert (moveData == null);
+
+            Map<Point, BuildingType> builds = buildData.getData();
+            if(builds.isEmpty())
+                result = false;
+            else {
+                result = true;
                 Iterator<Point> iterator = builds.keySet().iterator();
                 Point p = iterator.next();
-                while(iterator.hasNext())
-                    if(!iterator.next().equals(p))
-                        return false;
-                result = true;
+                while (iterator.hasNext()) {
+                    if (!iterator.next().equals(p)) {
+                        result = false;
+                    }
+                }
             }
-            else if (buildData == null){
-                return false;
-            }
+
 
             if(statement.getType() == StatementType.NIF)
                 result = !result;
@@ -246,8 +408,29 @@ public class StatementCompiler {
         return  lambdaStatement;
     }
 
-    private static boolean adiacent(Point p1, Point p2){
+    public static boolean adiacent(Point p1, Point p2){
         return (p2.x == p1.x && p2.y == p1.y - 1) || (p2.x == p1.x && p2.y == p1.y + 1) || (p2.x == p1.x - 1 && p2.y == p1.y) || (p2.x == p1.x + 1 && p2.y == p1.y) || (p2.x == p1.x + 1 && p2.y == p1.y + 1) || (p2.x == p1.x + 1 && p2.y == p1.y - 1) || (p2.x == p1.x - 1 && p2.y == p1.y - 1) || (p2.x == p1.x - 1 && p2.y == p1.y + 1);
+    }
+
+    public static List<Integer> getMoveDeltas(InternalModel model, List<Point> moves, MoveData moveData){
+        List<Integer> result = new ArrayList<>();
+
+        Map<LevelType,Integer> levelHeights = new HashMap<>();
+        levelHeights.put(LevelType.GROUND, 0);
+        levelHeights.put(LevelType.FIRST_FLOOR, 1);
+        levelHeights.put(LevelType.SECOND_FLOOR, 2);
+        levelHeights.put(LevelType.THIRD_FLOOR, 3);
+        levelHeights.put(LevelType.DOME, 4);
+
+        result.add(levelHeights.get(model.getBoard().getCell(moves.get(0)).getTopBuilding()) - levelHeights.get(model.getBoard().getCell(moveData.getWorker().getPosition()).getTopBuilding()));
+
+
+        for(int i = 0; i< moves.size()-1; i++){
+            int differenceInHeight = levelHeights.get(model.getBoard().getCell(moves.get(i+1)).getTopBuilding()) - levelHeights.get(model.getBoard().getCell(moves.get(i)).getTopBuilding());
+            result.add(differenceInHeight);
+        }
+
+        return result;
     }
 
 }
