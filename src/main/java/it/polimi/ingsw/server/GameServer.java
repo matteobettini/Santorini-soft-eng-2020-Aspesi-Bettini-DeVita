97ace2d9781b866355e1d4656b0927888361e3e5
MirkoDeVita98 <mirko.devita@icloud.com>
2020-05-24 13:04:55 +0200

    Inserted command line arguments for server and client.

--- src/main/java/it/polimi/ingsw/server/GameServer.java
+++ src/main/java/it/polimi/ingsw/server/GameServer.java
@@ -10,23 +10,46 @@ import java.io.IOException;
 
 public class GameServer {
 
+    private static final String HELP_ARGUMENT = "--help";
+    private static final int DEFAULT_PORT = 4567;
+    private static final int MIN_PORT = 1024;
+    private static final int MAX_PORT = 65535;
+
     public static void main(String[] args) throws IOException {
+
+        int port = DEFAULT_PORT;
+
+        if(args.length == 1 && HELP_ARGUMENT.equals(args[0])){
+           System.out.println("Insert a port number between " + MIN_PORT + " and " + MAX_PORT + " as argument or don't insert anything to start the server on the port " + DEFAULT_PORT + ".");
+           return;
+        }
+        else if(args.length == 1){
+            boolean error = false;
+
+            try{
+                int proposedPort = Integer.parseInt(args[0]);
+                if(proposedPort >= MIN_PORT && proposedPort <= MAX_PORT)
+                    port = proposedPort;
+                else error = true;
+            }catch (NumberFormatException ignored){
+                error = true;
+            }
+
+            if(error){
+                System.out.println("Invalid port number, insert " + HELP_ARGUMENT + " to see the available port numbers.");
+                return;
+            }
+        }
+        else if(args.length > 1) {
+            System.out.println("Too many arguments, insert " + HELP_ARGUMENT + " to see the available configuration options.");
+            return;
+        }
 
         try {
             CardFactory.getInstance();
         } catch (InvalidCardException e) {
             System.err.println("[" + e.getClass().toString() + "]" + e.getMessage());
             return; //Do not load the server if there are errors with the cards
-        }
-
-        int port = 4567;
-
-        if(args.length > 0){
-            try{
-                int proposedPort = Integer.parseInt(args[0]);
-                if(proposedPort >= 1024 && proposedPort <= 65535)
-                    port = proposedPort;
-            }catch (NumberFormatException ignored){ }
         }
 
         Server server = new ServerImpl(port);
